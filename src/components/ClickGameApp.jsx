5a0ffba29eef738b4e8a43d800358c00ff261b00
Unmani Shinde <mailto.unmani@gmail.com>
2023-11-22 20:17:26 +0530

    updated style for click page

--- src/components/ClickGameApp.jsx
+++ src/components/ClickGameApp.jsx
@@ -21,6 +21,7 @@ const ClickGameHome = () => {
   const [transactionInProgress,setTransactionInProgress] = useState(false);
   const [loading, setLoading] = useState(false);
   const [updating, setUpdating] = useState(false);
+  const [showBorder,setShowBorder] = useState(false);
 
   const nav = useNavigate();
   
@@ -57,10 +58,12 @@ const ClickGameHome = () => {
   const fetch = async () => {
     if (!account) return;
     try {
+      
       const countResource = await provider.getAccountResource(
         account.address,
         `${moduleAddress}::ClickApt::GlobalCount`,
       );
+      
 
       const globalResource = await provider.getAccountResource(
         hostAddress,
@@ -118,71 +121,14 @@ const ClickGameHome = () => {
     }
     finally {
       setTransactionInProgress(false);
+      setUpdating(false);
     }
   };
 
-  // useEffect(() => {
-  //   if (connected) {
-  //     // Call the smart contract function to get the total clicks
-  //     getTotalClicks();
-  //   }
-  // }, [connected]);
-
-
-// const handleIncrementClicks = async () => {
-//     if (!account) return []; // Assuming 0 as the default total clicks if there's no account
-  
-//     // Change this to be your module account address
-//     const clickcount = 1;
-    
-//     const payload = {
-//       type: "entry_function_payload",
-//       function: '0x8458e011098a8719842e1159a3005f405008244f6e9656fda6fb7e903265affb::click_apt_addr::clickapt::increment_clicks',
-//       type_arguments: [],
-//       arguments: [account, clickcount],
-//     };
-    
-
-//     console.log(payload);
-//     try {
-//       setTransactionInProgress(true);
-//       const response = await signAndSubmitTransaction(payload);
-//       await client.waitForTransaction(response);
-      
-//       // const ClickGameResource = await provider.getAccountResource(
-//       //   account.address,
-//       //   `${moduleAddress}::clickapt::ClickGame`
-//       // );
-//       // setTotalClicks(5);      
-//       // // Assuming that the resource contains a field 'total_clicks', adjust it accordingly
-//       // const totalClicks = ClickGameResource.total_clicks;
-//       // setTotalClicks(totalClicks);
-//     } catch (error) {
-//       console.log('Error in Increment:',error);
-//     }
-//     finally{
-//       setTransactionInProgress(false);
-//     }
-//   };
-  
-
-//   const handleIncrementClicks = async () => {
-//     try {
-//       const clicks = 1; // You can adjust the number of clicks as needed
-//       await window.ClickGame.increment_clicks(account, clicks);
-//       getTotalClicks(); // Update the total clicks after incrementing
-//     } catch (error) {
-//       console.error('Error incrementing clicks:', error);
-//     }
-//   };
-
-
-  
-
   return (
-    <div>
+    <div style={{height:"100%",width:'100%',overflowX:'hidden',display:'flex',flexDirection:'column'}}>
     
-      <Layout>
+      <Layout style={{maxHeight:"20vh",backgroundColor:'transparent'}}>
         <Row align="middle">
           <Col flex={"auto"}>
             <h1>Click Game ({network?.name.toString()})</h1>
@@ -192,93 +138,111 @@ const ClickGameHome = () => {
           </Col>
         </Row>
       </Layout>
-      {!connected && <Alert message={`Please connect your wallet`} type="info" />}
+      {!connected && <Alert style={{width:"50%",justifyContent:'center',alignSelf:"center",marginTop:"30vh",backgroundColor:'transparent',border:'solid 2px white',color:'white'}} message={`Please connect your wallet`} type="info" />}
       {connected && (network?.name.toString()).toLowerCase() !== NETWORK && (
         <Alert
           message={`Wallet is connected to ${network?.name}. Please connect to ${NETWORK}`}
           type="warning"
+          
         />
       )}
-      {connected && (network?.name.toString()).toLowerCase() === NETWORK && (
-         <Button onClick={()=>{nav(`/leaderboard`)}}style={{marginTop:'9vh'}}disabled={!account || transactionInProgress}>View LeaderBoard</Button>
-      )}
-      {connected && !(loading) && (network?.name.toString()).toLowerCase() === NETWORK && (
+      <div style={{display:'flex',flexDirection:'column',width:'100%',height:"50vh"}}>
+      
+      {/* {connected && (network?.name.toString()).toLowerCase() === NETWORK && (
+         <Button onClick={()=>{nav(`/leaderboard`)}}style={{marginTop:'-5vh',width:"20%",alignSelf:'center',backgroundColor:'transparent',fontWeight:'700',border:"transparent",fontSize:"1.75vw"}}disabled={!account || transactionInProgress}><u>View LeaderBoard</u></Button>
+      )} */}
+
+        <div style={{display:'flex',flexDirection:'row',width:'100%'}}>
+          <div style={{width:"49%",marginLeft:'1vw',marginTop:'1vh',height:"100%",color:'white',paddingTop:loading||updating?"4vh":'10vh',border:connected?'solid 2px black':'transparent',borderRadius:"10px"}}>
+            <div style={{margin:'1vh',width:"95%",height:"100%"}}>
+              {connected && (<h3 style={{fontWeight:'700'}}>Hello there! I'm the MoveButton. Out of my many forms, currently you're seeing me in the <u>APTOS {network?.name}</u></h3>)}
+              {connected && (<h3 style={{ marginTop: '-3vh' }}>You're Currently Connected to: 🔏 <u>{ account?.address ? `${account?.address.substring(0, 6)}...${account?.address.slice(-4)}` : 'N/A'}</u></h3> )}
+                   
+          {connected && !(loading) && (network?.name.toString()).toLowerCase() === NETWORK && (
         <Spin spinning={transactionInProgress}>
-          <h3>Hello! The Network Name is: {network?.name}</h3>
-          <h3>Universal Count: {universalCount}</h3>          
-          <h3>User's Count:{data}</h3>
-          <h3>Account:{account?.address}</h3>
-         <Button disabled={!account || transactionInProgress} onClick={incrementButton}>Increment Counter</Button>
+          <h3>These are your Stats:</h3>
+          <h3>Number of Clicks across the Globe: 🌍 {universalCount}</h3>          
+          <h3>Number of Clicks by You: 🙋🏻‍♂️ {data}</h3>
+          
         </Spin>
       )}
-
-      
-
       {connected && (loading) && (network?.name.toString()).toLowerCase() === NETWORK && (
         <div style={{display:'flex',flexDirection:'column'}}>
-          <p>Reacquiring data...</p>
+          <p style={{color:'black'}}>Refreshing...</p>
           <div className="loader"></div>
         </div>
         
       )}
 
-      {connected && (updating) && (!transactionInProgress) (network?.name.toString()).toLowerCase() === NETWORK && (
+      {connected && (updating) && !transactionInProgress && (network?.name.toString()).toLowerCase() === NETWORK && (
         <div style={{display:'flex',flexDirection:'column'}}>
-        <p>Updating data...</p>
+        <p style={{color:'black'}}>Updating data...</p>
         <div className="loader"></div>
       </div>
       )}
 
+          </div>
+         
+         
 
+
+          </div>
+
+          <div style={{width:"50%",height:"100%",}}>
+
+{connected && (network?.name.toString()).toLowerCase() === NETWORK && (
+<div>
+  <h3 style={{fontWeight:'700',marginTop:'7vh',marginBottom:'-7vh'}}>Click Me to Increment!</h3>
+
+<Button
+  disabled={!account || transactionInProgress}
+  onClick={incrementButton}
+  style={{
+    width: '20vw',
+    height: '20vw',
+    borderRadius: '100%',
+    alignSelf:'center',
+    justifyContent:'center',
+    marginTop:'10vh',
+    border:'solid 2px white',
+    background: '#1640D6',
+    fontSize: '18px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.3)',
+  }}
+>
   
-      {/* Uncomment the following code if needed */}
-      {/* <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', width: '100%', alignSelf: 'center', alignItems: 'center' }}>
-        {connected ? (
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', alignSelf: 'center', fontSize: '1.4vw' }}>
-            <b>Connected Account Address:</b> {account?.address}
-            <p>Total Clicks: {totalClicks}</p>
-            <button className="disconnect-button" onClick={disconnect}>
-              Disconnect
-            </button>
-          </div>
-        ) : (
-          <div>
-            <WalletSelector />
-          </div>
-        )}
-        <button style={{ width: '29%', marginTop: '7vh', padding: '1vw', fontSize: '1.5vw' }} onClick={handleViewConnectionStatus}>
-          View Connection Status
-        </button>
-        <Spin spinning={transactionInProgress}>
-          <Button type="primary" onClick={getTotalClicks}>
-            Increment Clicks
-          </Button>
-        </Spin>
-      </div> */}
+</Button>
+
+
+</div>
+
+
+
+)}
+
+
+
+
+  </div>
+        </div>
+      
+
+
+      
+
+
+
+      
+
+      
+
+
+      </div>
     </div>
   );
   
 
-  // return (
-  //   <div style={{ display: "flex", flexDirection: 'column', justifyContent: 'center', height: '100%', width: "100%", alignSelf: 'center', alignItems: 'center' }}>
-  //     {connected ? (
-  //       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', alignSelf: 'center', fontSize: "1.4vw" }}>
-  //         <b>Connected Account Address:</b> {account?.address}
-  //         <p>Total Clicks: {totalClicks}</p>
-  //         {/* <button onClick={handleIncrementClicks}>Increment Clicks</button> */}
-  //         <button className='disconnect-button' onClick={disconnect}>Disconnect</button>
-  //       </div>
-  //     ) : (
-  //       <div>
-  //         <WalletSelector />
-  //       </div>
-  //     )}
-  //     <button style={{ width: "29%", marginTop: '7vh', padding: '1vw', fontSize: '1.5vw' }} onClick={handleViewConnectionStatus}>View Connection Status</button>
-  //     <Spin spinning={transactionInProgress}>
-  //       <Button type="primary" onClick={()=>getTotalClicks()}>Increment Clicks</Button>
-  //     </Spin>
-  //   </div>
-  // );
+  
 };
 
 export default ClickGameHome;
